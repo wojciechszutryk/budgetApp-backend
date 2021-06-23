5a25dadf0df342f3dad05868f4efdb73089dd186
szutryk <wojtekszutryk@gmail.com>
2021-06-23 12:22:27 +0200

    change app and server names

--- app.js
+++ app.js
@@ -1,63 +1,8 @@
-const express = require('express');
-const app = express();
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config();
+const http = require('http');
+const server = require('./server');
 
-const usersRoutes = require('./routes/users')
-const parentCategoriesRoutes = require('./routes/parentCategories')
-const categoriesRoutes = require('./routes/categories')
-const budgetsRoutes = require('./routes/budgets')
-const budgetCategoriesRoutes = require('./routes/budgetCategories')
-const transactionsRoutes = require('./routes/transactions')
+const port = process.env.PORT || 5000;
 
+const serv = http.createServer(server);
 
-mongoose.connect('mongodb+srv://admin:'+process.env.MONGODB_PW+'@cluster0.ooyuy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
-mongoose.Promise = global.Promise;
-
-app.use(cors({
-    origin: "*"
-}))
-app.use(morgan('dev'));
-app.use('/uploads', express.static('uploads'));
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-
-app.use((req, res,next)=>{
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    if (req.method === 'OPTIONS'){
-        res.header('Access-Control-Allow-methods', 'GET,PUT,POST,PATCH,DELETE');
-        return  res.status(200).json({});
-    }
-    next();
-})
-
-app.use('/users', usersRoutes);
-app.use('/parentCategories', parentCategoriesRoutes);
-app.use('/categories', categoriesRoutes);
-app.use('/budgetCategories', budgetCategoriesRoutes);
-app.use('/transactions', transactionsRoutes);
-app.use('/budgets', budgetsRoutes);
-
-app.use((req, res,next) => {
-    const error =new Error('not found');
-    error.status = 404;
-    next(error);
-});
-
-app.use((error, req,res,next) => {
-    res.status(error.status || 500);
-    res.json({
-        error: {
-            message: error.message
-        }
-    })
-})
-
-module.exports = app;
+serv.listen(port);
--- server.js
+++ server.js
@@ -1,8 +1,63 @@
-const http = require('http');
-const app = require('./app');
+const express = require('express');
+const server = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
 
-const port = process.env.PORT || 5000;
+const usersRoutes = require('./routes/users')
+const parentCategoriesRoutes = require('./routes/parentCategories')
+const categoriesRoutes = require('./routes/categories')
+const budgetsRoutes = require('./routes/budgets')
+const budgetCategoriesRoutes = require('./routes/budgetCategories')
+const transactionsRoutes = require('./routes/transactions')
 
-const server = http.createServer(app);
 
-server.listen(port);
+mongoose.connect('mongodb+srv://admin:'+process.env.MONGODB_PW+'@cluster0.ooyuy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+mongoose.Promise = global.Promise;
+
+server.use(cors({
+    origin: "*"
+}))
+server.use(morgan('dev'));
+server.use('/uploads', express.static('uploads'));
+server.use(bodyParser.urlencoded({extended: false}));
+server.use(bodyParser.json());
+
+server.use((req, res, next)=>{
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if (req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-methods', 'GET,PUT,POST,PATCH,DELETE');
+        return  res.status(200).json({});
+    }
+    next();
+})
+
+server.use('/users', usersRoutes);
+server.use('/parentCategories', parentCategoriesRoutes);
+server.use('/categories', categoriesRoutes);
+server.use('/budgetCategories', budgetCategoriesRoutes);
+server.use('/transactions', transactionsRoutes);
+server.use('/budgets', budgetsRoutes);
+
+server.use((req, res, next) => {
+    const error =new Error('not found');
+    error.status = 404;
+    next(error);
+});
+
+server.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+})
+
+module.exports = server;
