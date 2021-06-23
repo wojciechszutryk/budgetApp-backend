54c27c8783bb0766225e064631cd9a8e6ac0a83c
szutryk <wojtekszutryk@gmail.com>
2021-06-23 12:37:11 +0200

    change .env

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const server = require('./server');
 
-const port = process.env.PORT || 5000;
+const port = process.env.SERVER_URL || 5000;
 
 const serv = http.createServer(server);
 
